2ee7e288007b5913c58223519cb021d033073686
Han <liuhan132@foxmail.com>
2018-04-01 16:11:57 +0800

    draw loss with train and dev

--- utils/analysis_log.py
+++ utils/analysis_log.py
@@ -4,26 +4,11 @@ import matplotlib.pyplot as plt
 import re
 
 
+TRAIN_SIZE = 87599
+DEV_SIZE = 10570
+
+
 def analysis_log_loss(log_txt):
-    epoch_loss = []
-    p = re.compile(r'.*epoch=(\d*), batch=\d*.\d*, loss=(\d*\.\d*).*')
-    for line in log_txt:
-        result = re.findall(p, line)
-        if len(result) == 0:
-            continue
-
-        tmp_epoch_loss = result[0]
-        epoch = int(tmp_epoch_loss[0])
-        loss = float(tmp_epoch_loss[1])
-
-        if len(epoch_loss) < epoch + 1:
-            epoch_loss.append(0.)
-        else:
-            epoch_loss[epoch] += loss
-    return epoch_loss
-
-
-def analysis_log_sum_loss(log_txt):
     epoch = []
     loss = []
     p = re.compile(r'.*epoch=(\d*), sum_loss=(\d*\.\d*).*')
@@ -33,7 +18,7 @@ def analysis_log_sum_loss(log_txt):
             continue
 
         epoch.append(int(result[0][0]))
-        loss.append(float(result[0][1]))
+        loss.append(float(result[0][1])/TRAIN_SIZE)
 
     return epoch, loss
 
@@ -52,48 +37,40 @@ def analysis_log_score(log_txt):
         epoch.append(int(result[0][0]))
         score_em.append(float(result[0][1]))
         score_f1.append(float(result[0][2]))
-        loss.append(float(result[0][3]))
+        loss.append(float(result[0][3])/DEV_SIZE)
 
     return epoch, score_em, score_f1, loss
 
 
-def main_loss():
-    with open('../logs/7-debug.log') as f_log:
-        log_lines = f_log.readlines()
-        value_log = log_lines
-
-    epoch, epoch_loss = analysis_log_sum_loss(value_log)
-    for i, sum_loss in zip(epoch, epoch_loss):
-        print('epoch=%d, sum_loss=%f' % (i, sum_loss))
+def draw_loss(epoch, train_loss, eval_loss):
+    for i, tl, el in zip(epoch, train_loss, eval_loss):
+        print('epoch=%d, train_loss=%f, eval_loss=%f' % (i, tl, el))
 
     # plot
     x = epoch
-    y = epoch_loss
+    y1 = train_loss
+    y2 = eval_loss
 
-    plt.plot(x, y, marker='o')
+    plt.figure()
+    plt.plot(x, y1, marker='o', color='b')
+    plt.plot(x, y2, marker='^', color='r')
 
     plt.xlabel('epoch')
-    plt.ylabel('sum_loss')
+    plt.ylabel('loss')
+    plt.legend(labels=['train', 'eval'])
     plt.grid()
-    plt.show()
 
 
-def main_score():
-    with open('../logs/7-debug.log') as f_log:
-        log_lines = f_log.readlines()
-        value_log = log_lines
-
-    epoch, score_em, score_f1, loss = analysis_log_score(value_log)
-    for i, em, f1, ls in zip(epoch, score_em, score_f1, loss):
-        print('epoch=%d, score_em=%.2f, score_f1=%.2f, sum_loss=%.5f' % (i, em, f1, ls))
+def draw_score(epoch, score_em, score_f1):
+    for i, em, f1 in zip(epoch, score_em, score_f1):
+        print('epoch=%d, score_em=%.2f, score_f1=%.2f' % (i, em, f1))
 
     # plot
     x = epoch
     y1 = score_em
     y2 = score_f1
-    y3 = loss
 
-    plt.figure(1)
+    plt.figure()
     plt.plot(x, y1, marker='o', color='b')
     plt.plot(x, y2, marker='^', color='r')
 
@@ -102,16 +79,22 @@ def main_score():
     plt.legend(labels=['em', 'f1'])
     plt.grid()
 
-    plt.figure(2)
-    plt.plot(x, y3, marker='^', color='g')
 
-    plt.xlabel('epoch')
-    plt.ylabel('sum_loss')
+def main(log_path):
+    with open(log_path) as f_log:
+        log_lines = f_log.readlines()
+        value_log = log_lines
 
-    plt.grid()
+    epoch, train_loss = analysis_log_loss(value_log)
+    epoch2, score_em, score_f1, eval_loss = analysis_log_score(value_log)
+
+    assert epoch == epoch2
+
+    draw_loss(epoch, train_loss, eval_loss)
+    draw_score(epoch, score_em, score_f1)
+
     plt.show()
 
 
 if __name__ == '__main__':
-    main_loss()
-    main_score()
+    main('../logs/match-lstm.log')
